037fc182bf932df83733edc86166e50bbdb5f5ce
0Armaan025 <armaan33000@gmail.com>
2023-11-20 22:18:19 +0530

    todo

--- perfumex/src/app/become_a_seller/become_seller.js
+++ perfumex/src/app/become_a_seller/become_seller.js
@@ -11,7 +11,8 @@ export default function BecomeSeller() {
 
                         <div className="leftBox">
                             <h1 className="sellerHeading">Become a seller!</h1>
-                            <h3 className='sellerSubHeading'>Would you like to become a seller on perfumex? Join this!</h3>
+                            <h3 className='sellerSubHeading'>Would you like to become a seller on perfumex?</h3>
+                            <button className='becomeSellerButton'>Let's do this!</button>
                         </div>
                         <div className='rightBox'>
                             <img src="https://plus.unsplash.com/premium_photo-1661774914180-08cc5695c539?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNlbGxlcnxlbnwwfDB8MHx8fDA%3D" className='sellerImage'/>
---
+++ perfumex/src/app/start_with_shopping/start_with_shopping.js
@@ -0,0 +1,3 @@
+export default function StartWithShopping() {
+    // TODO: MAKE THIS
+}

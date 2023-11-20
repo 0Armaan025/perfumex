5dce72d1322e802024a656561ba89907a2df96d5
0Armaan025 <armaan33000@gmail.com>
2023-11-20 21:59:23 +0530

    updated home

---
+++ perfumex/src/app/become_a_seller/become_seller.js
@@ -0,0 +1,26 @@
+import './becomeaseller.css';
+
+export default function BecomeSeller() {
+    return (
+        <>
+
+            <div className="becomeSellerDiv">
+                <br />
+
+                <div className='boxes'>
+
+                        <div className="leftBox">
+                            <h1 className="sellerHeading">Become a seller!</h1>
+                            <h3 className='sellerSubHeading'>Would you like to become a seller on perfumex? Join this!</h3>
+                        </div>
+                        <div className='rightBox'>
+                            <img src="https://plus.unsplash.com/premium_photo-1661774914180-08cc5695c539?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHNlbGxlcnxlbnwwfDB8MHx8fDA%3D" className='sellerImage'/>
+                        </div>
+
+
+                </div>
+            </div>
+
+        </>
+    )
+}

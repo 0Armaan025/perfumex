2c2a54bf591e032b58c774c1b46bc8cea28349ef
0Armaan025 <armaan33000@gmail.com>
2023-11-26 13:32:53 +0530

    fields updated

--- perfumex/src/components/footer/page.js
+++ perfumex/src/components/footer/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
         <>
@@ -12,7 +14,7 @@ export default function Footer() {
                         </a>
                         <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                             <li>
-                                <a href="#" className="hover:underline me-4 md:me-6">Sell on PerfumeX</a>
+                                <Link href="/become-a-seller" className="hover:underline me-4 md:me-6">Sell on PerfumeX</Link>
                             </li>
                             <li>
                                 <a href="#" className="hover:underline me-4 md:me-6">Giftcards</a>

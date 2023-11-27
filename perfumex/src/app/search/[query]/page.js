6b11bcc73d59347e06a4e22b23f88d35111aebd0
0Armaan025 <armaan33000@gmail.com>
2023-11-27 17:54:12 +0530

    common

---
+++ perfumex/src/app/search/[query]/page.js
@@ -0,0 +1,14 @@
+import RootLayout from "@/app/layout";
+import Navbar from "@/components/navbar/page";
+
+export default function SearchPage() {
+    return (
+        <>
+            <RootLayout>
+                <main>
+                    <Navbar/>
+                </main>
+            </RootLayout>
+        </>
+    );
+}
